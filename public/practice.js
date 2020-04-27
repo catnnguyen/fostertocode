16887ca028c0999bb921b900a4dca0895ae77687
cn5-cos316 <cn5@princeton.edu>
2020-04-26 17:02:45 -0700

    new files

---
+++ public/practice.js
@@ -0,0 +1,93 @@
+
+const droppable = new Droppable.default(document.querySelectorAll('.container'), {
+    draggable: '.item',
+    dropzone: '.dropzone'
+  
+  });
+  
+  let droppableOrigin;
+  
+  // --- Draggable events --- //
+  droppable.on('drag:start', (evt) => {
+    droppableOrigin = evt.originalSource.parentNode.dataset.dropzone;
+  });
+  
+  droppable.on('droppable:dropped', (evt) => {
+    if (droppableOrigin !== evt.dropzone.dataset.dropzone) {
+      evt.cancel();
+    }
+  
+   
+  });
+  
+  function changeColor(){
+    document.getElementById("cat").style.backgroundColor = "lightgreen";
+  }
+  
+  
+  function changeToCat() {
+    const text = document.getElementById("catherine").innerHTML;
+    if (text === "4") {
+      document.getElementById("catherine").innerHTML = "Catherine was here";
+    } else {
+      document.getElementById("catherine").innerHTML = "4";
+    }
+  }
+  
+  function showStep2() {
+      document.getElementById("step2").style.visibility = "visible";
+  }
+  function showStep2b() {
+      document.getElementById("step2b").style.visibility = "visible";
+  }
+  
+  function showStep3(){
+      document.getElementById("step3").style.visibility = "visible";
+  }
+  
+  function showStep3b(){
+      document.getElementById("step3b").style.visibility = "visible";
+  }
+  
+  function showStep3c(){
+      document.getElementById("step3c").style.visibility = "visible";
+  }
+  
+  function showStep4(){
+      document.getElementById("step4").style.visibility = "visible";
+  }
+  
+  function showStep4b(){
+      document.getElementById("step4b").style.visibility = "visible";
+  }
+  
+  function showStep4c(){
+      document.getElementById("step4c").style.visibility = "visible";
+  }
+  function showStep4d(){
+      document.getElementById("step4d").style.visibility = "visible";
+  }
+  
+  function showStep4e(){
+      document.getElementById("step4e").style.visibility = "visible";
+  }
+  
+  function showStep5(){
+      document.getElementById("step5").style.visibility = "visible";
+  }
+  
+  function showStep5b(){
+      document.getElementById("step5b").style.visibility = "visible";
+  }
+  
+  function showStep5c(){
+      document.getElementById("step5c").style.visibility = "visible";
+  }
+  
+  function showStep5d(){
+      document.getElementById("step5d").style.visibility = "visible";
+  }
+  
+  function showStep5e(){
+      document.getElementById("step5e").style.visibility = "visible";
+  }
